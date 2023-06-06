d4ce8edd01ca1b276d583075705d81071f2008f5
EricLiWSL <221502012@smail.nju.edu.cn>
2023-06-06 22:05:40 +0800

    [new] Config

---
+++ Config/AddTeXLiveToPATH.sh
@@ -0,0 +1,4 @@
+echo 'PATH=/usr/local/texlive/2023/bin/x86_64-linux:$PATH' >> ~/.bashrc
+echo 'MANPATH=/usr/local/texlive/2023/texmf-dist/doc/man:$MANPATH' >> ~/.bashrc
+echo 'INFOPATH=/usr/local/texlive/2023/texmf-dist/doc/info:$INFOPATH' >> ~/.bashrc
+source ~/.bashrc
