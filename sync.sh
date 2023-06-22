7058f35482df4e989ba5a1d60d44e511e01d6d41
EricUbuntu <3270793295@qq.com>
2023-06-22 11:19:56 +0800

    fix

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 echo $(date +"%Y-%m-%d %H:%M:%S") | tee -a sync.log
 git pull gitlab_nju main | tee -a sync.log
-git push github_nju main | tee -a sync.log
-git pull gitlab main | tee -a sync.log
+git push gitlab_nju main | tee -a sync.log
+git pull github main | tee -a sync.log
 git push github main | tee -a sync.log
-git pull github_my main | tee -a sync.log
+git pull gitlab_my main | tee -a sync.log
 git push gitlab_my main | tee -a sync.log
